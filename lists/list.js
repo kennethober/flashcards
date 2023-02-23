ce8a49dfb2b0091bb201ba42a66fab46313bdcf0
threyaober <115948745+threyaober@users.noreply.github.com>
2023-02-22 21:58:32 -0500

    Update list.js

--- lists/list.js
+++ lists/list.js
@@ -6,7 +6,7 @@
 //
 //  To deactivate a single line from the list below, precede it with '//', like this:
 //  // ['english', 'korean'],
-//
+// 
 //  To deactivate a section from the list below, delimit it with '/*' and '*/', like this:
 //  /*
 //  ['english1', 'korean1'],
