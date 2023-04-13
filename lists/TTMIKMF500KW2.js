66ac143aa09c032001a7d393cc36b3645aebf5fe
threyaober <115948745+threyaober@users.noreply.github.com>
2023-04-13 11:36:45 -0400

    Update TTMIKMF500KW2.js

--- lists/TTMIKMF500KW2.js
+++ lists/TTMIKMF500KW2.js
@@ -14,9 +14,24 @@
 //  */
 
 const listFromFile = [
-  ['e1', 'k1'],
-  ['e2', 'k2'],
-  ['e3', 'k3'],
-  ['e4', 'k4'],
-  ['e5', 'k5']
+  ['Eating out, dining out', '외식'],
+  ['To be various, to be diverse', '다양하다'],
+  ['Garlic', '마늘'],
+  ['To fry, to stir-fry', '볶다'],
+  ['Dessert', '후식'],
+  ['To be sweet', '달콤하다'],
+  ['Night view, nightscape', '야경'],
+  ['Previous engagement, prior appointment', '선약'],
+  ['To cancel', '취소하다'],
+  ['To be excellent, to be great', '훌륭하다'],
+  ['Visa', '비자'],
+  ['Issue', '발급'],
+  ['Overseas trip', '해외여행'],
+  ['Schedule, itinerary', '일정'], 
+  ['Party, company, group', '일행'],
+  ['Tradition', '전통'],
+  ['Experience, hands-on experience, activity', '체험'],
+  ['To suggest, to propose', '제안하다'],
+  ['To leave the country, to depart', '출국하다'],
+  ['To pack', '싸다'],
 ]
