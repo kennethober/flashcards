9f65c0c0855d0c0a80528066c7f588229b57c786
threyaober <115948745+threyaober@users.noreply.github.com>
2024-01-11 16:08:51 -0500

    Update 9HeartDiary.js

--- lists/9HeartDiary.js
+++ lists/9HeartDiary.js
@@ -26,5 +26,21 @@ const listFromFile = [
   ['Cause, reason', '때문'],
   ['Because of, due to', '때문에'],
   ['Meaning', '뜻'],
-  ['Emotion', '감정']
+  ['Emotion', '감정'],
+  ['Seed', '씨앗'],
+  ['To plant', '심다'],
+  ['Potted plant', '화분'],
+  ['Sprout, shoot', '싹'],
+  ['To sprout, grow', '돋다'],
+  ['Clearly, distinctly', '똑똑히'],
+  ['Running competition', '달리기 시험'],
+  ['Finishing in last place, coming in last', '꼴찌'],
+  ['Doing something consistently, repeatedly', '-만 하다'],
+  ['Finally', '드디어'],
+  ['To feel proud, satisfied', '뿌듯하다'],
+  ['To be happy, glad', '기쁘다'],
+  ['Heart', '가슴'],
+  ['To be sentimental, choked up', '뭉클하다'],
+  ['Particle used when listing out examples or choices (at least, there can be more than mentioned)', '이나'],
+  ['Conjunction meaning or', '거나']
 ]
